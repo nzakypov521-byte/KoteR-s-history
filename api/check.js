91b6394089ac27eb3de23c1ef32deba9ef402102
Нурэль <nzhakypov521@gmail.com>
2026-05-06 12:20:56 +0600

    remove debug

--- api/check.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-    const key = process.env.GROQ_API_KEY
-    res.json({
-      exists: !!key,
-      length: key?.length,
-      prefix: key?.slice(0, 8),
-    })
-  }
